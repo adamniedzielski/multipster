019adbcf30c38b97a6a5f1be9b2abe84e79603cb
Adam Niedzielski <adamsunday@gmail.com>
2018-01-23 22:58:03 +0100

    Bump Elixir version to 1.6.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Multipster.Mixfile do
     [
       app: :multipster,
       version: "0.1.0",
-      elixir: "~> 1.5.3",
+      elixir: "~> 1.6.0",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
